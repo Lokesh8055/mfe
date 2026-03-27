b5963a064eb89c365f50b64cba563f2554ce71a2
lokesh8055 <lokeshpadmanabhan98@gmail.com>
2026-03-27 11:52:47 +0530

    Added Header component in container mfe

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,18 +1,14 @@
 import React, { useEffect, useRef } from 'react';
-import { mountMarketing } from 'marketing/MarketingApp';
+import { BrowserRouter } from 'react-router-dom';
+import Marketing from './components/Marketing';
+import Header from './components/Header';
 
 const App = () => {
-  const marketingRef = useRef(null);
-
-  useEffect(() => {
-    mountMarketing(marketingRef.current);
-  }, []);
-
   return (
-    <>
-      <h1>Hello Container!</h1>
-      <div ref={marketingRef}></div>
-    </>
+    <BrowserRouter>
+      <Header />
+      <Marketing />
+    </BrowserRouter>
   );
 }
 
---
+++ packages/container/src/components/Marketing.js
@@ -0,0 +1,15 @@
+import { mountMarketing } from 'marketing/MarketingApp';
+
+const Marketing = () => {
+  const marketingRef = useRef(null);
+
+  useEffect(() => {
+    mountMarketing(marketingRef.current);
+  }, []);
+
+  return (
+    <div ref={marketingRef}></div>
+  );
+}
+
+export default Marketing;
