2637eb02229f39d451c650a80c7dd054b8692202
lokesh8055 <lokeshpadmanabhan98@gmail.com>
2026-03-29 17:28:14 +0530

    use browser history in isolation on marketing app

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mountMarketing = (el, { onNavigate } = {}) => {
-  const history = createMemoryHistory();
+const mountMarketing = (el, { onNavigate, defaultHistory } = {}) => {
+  const history = defaultHistory || createMemoryHistory();
   if (onNavigate) {
     history.listen(onNavigate);
   }
@@ -31,7 +31,7 @@ const mountMarketing = (el, { onNavigate } = {}) => {
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
   if (devRoot) {
-    mountMarketing(devRoot);
+    mountMarketing(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
